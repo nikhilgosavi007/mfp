ba68e1883b6149b8c8c2882c880c8969b2750880
nikhil <nikhil.gosavi@pentacor.de>
2021-07-11 08:33:34 +0200

    marketing integrated in container

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,32 @@
+// merge can be used to merge 2 webpack configs
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8081,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            //shared: packageJson.dependencies
+        })
+    ]
+};
+
+// assigning devConfig seconds means that any of the common properties will be overwritten by devConfig
+module.exports = merge(commonConfig, devConfig);
